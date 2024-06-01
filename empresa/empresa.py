3f11b79db8000d6befadcf1b9369667c37e48fba
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-06-01 11:53:20 -0300

    Update empresa.py

--- empresa/empresa.py
+++ empresa/empresa.py
@@ -123,7 +123,10 @@ class PredictEmprestimo(object):
         return df3[boruta_columns]
 
     def get_predictions(self, model, test_data, original_data):
-        return '[{"idade": 25, "renda": 162500, "posse_casa": "RENT", "tempo_emprego": 2.0, "finalidade_emprestimo": "VENTURE", "grau_risco_emprestimo": "A", "valor_emprestimo": 35000, "taxa_juros_emprestimo": 7.49, "status_emprestimo": 0, "relacao_emprestimo_renda": 0.22, "registro_inadimplencia": "N", "historico_credito": 4}]'
-        
+        print('predict')
+        pred = model.predict(test_data)
+        original_data['prediction'] = pred.values
+        print('predict3')
+        return original_data.to_json(orient='records', date_format='iso')
     
 
