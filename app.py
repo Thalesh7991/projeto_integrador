7ad09ae01d9760d735dc0756b56484ffcf7d3522
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-06-06 12:34:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def emprestimo_predict():
         df_preparation = pipeline.data_preparation(df_feature)
         print('FIM PREPARATION')
         print('INICIO PREDICT')
-        df_predict = pipeline.get_predictions(model, df_preparation)
+        
         print('FIM PREDICT')
-        return df_predict
+        return df_preparation
     else:
         return Response('{}', status=200, mimetype='application/json')
 
